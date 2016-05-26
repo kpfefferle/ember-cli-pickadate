f9f0089398a109d3ba9df1ab15ea8abaa95d00b0
Kevin Pfefferle <kevin@pfefferle.co>
2016-05-26 15:10:09 -0400

    Don't import JS in Fastboot

--- index.js
+++ index.js
@@ -13,9 +13,11 @@ module.exports = {
       options.theme = 'default';
     }
 
-    app.import(app.bowerDirectory + '/pickadate/lib/picker.js');
-    app.import(app.bowerDirectory + '/pickadate/lib/picker.date.js');
-    app.import(app.bowerDirectory + '/pickadate/lib/picker.time.js');
+    if (!process.env.EMBER_CLI_FASTBOOT) {
+      app.import(app.bowerDirectory + '/pickadate/lib/picker.js');
+      app.import(app.bowerDirectory + '/pickadate/lib/picker.date.js');
+      app.import(app.bowerDirectory + '/pickadate/lib/picker.time.js');
+    }
     app.import(app.bowerDirectory + '/pickadate/lib/themes/' + options.theme + '.css');
     app.import(app.bowerDirectory + '/pickadate/lib/themes/' + options.theme + '.date.css');
     app.import(app.bowerDirectory + '/pickadate/lib/themes/' + options.theme + '.time.css');
